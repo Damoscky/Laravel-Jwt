1ed9db8f607f321115778d7bf03ae83d5a1549ad
Akinwunmi Damilola <adamilola@sbsc.com>
2020-08-01 09:21:54 +0100

    Articles with Vue

--- laravel/package/contact/src/ContactServiceProvider.php
+++ laravel/package/contact/src/ContactServiceProvider.php
@@ -13,6 +13,7 @@ class ContactServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->loadViewsFrom(__DIR__. '/views', 'contact');
+        $this->loadMigrationsFrom(__DIR__. '/database/migrations');
     }
 
     public function register()
--- laravel/package/contact/src/database/migrations/2020_07_30_002716_create_contacts_table.php
+++ laravel/package/contact/src/database/migrations/2020_07_30_002716_create_contacts_table.php
@@ -15,6 +15,9 @@ class CreateContactsTable extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->mediumText('message')->nullable();
             $table->timestamps();
         });
     }
